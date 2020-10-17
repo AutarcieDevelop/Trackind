9068a4b3d5d0c6c5f868aa9c166128930f628127
Mohamed El Jelali <55602247+AutarcieDevelop@users.noreply.github.com>
2020-10-17 20:28:26 +0200

    init2

--- functions.js
+++ functions.js
@@ -19,4 +19,27 @@ module.exports = {
             return getCoords(macs);
           });
     },
+    getCoords: function(macAddresses) {
+      var body = {
+        "considerIp": "false",
+        "wifiAccessPoints": macAddresses
+      }
+      fetch("https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY", {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        //be sure to serialize the value of the body
+        body: JSON.stringify(body)
+      })
+      .then(res=>res.json())
+      .then((json, err)=>{
+        if (err){
+          console.log('Error: ' + err)
+        } else {
+          console.log(json)
+        }
+      })
+    },
 }
